1057d7feb1ac88c410b4cd5e15dbc17469598fd4
Ferenc Kovács <kovfer89@gmail.com>
2018-10-06 12:10:42 +0200

    harmasdia

---
+++ HelloWorld/src/HarmasDia.java
@@ -0,0 +1,13 @@
+
+public class HarmasDia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a= Integer.parseInt(args[0]);
+		if (a%2==0) {
+			
+		}
+	}
+
+}
