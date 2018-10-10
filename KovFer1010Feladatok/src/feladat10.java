7d102f8a14c0cfd28397b9dd46ce8842ef6a6194
Ferenc Kovács <kovfer89@gmail.com>
2018-10-10 18:58:20 +0200

    9,10.feladat

---
+++ KovFer1010Feladatok/src/feladat10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
+
+public class feladat10 {
+
+	public static void main(String[] args) {
+		System.out.println("10.feladat:"
+				+ "\n\tRajzoltass a képernyőre csillagokkal olyan téglalapot, "
+				+ "\n\tmelynek magasságát és szélességét paraméterben adhatjuk meg. "
+				+ "\n\tHa nem adunk meg paramétereket, az alapértelmezett magasság "
+				+ "\n\tés szélesség 5 csillag.");
+
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Kérem a téglalap magasságát: ");
+		int mag=sc.nextInt();
+		System.out.print("Kérem a téglalap szélességét: ");
+		int szel=sc.nextInt();
+	
+		for (int i = 0; i < mag; i++) {
+			for (int j = 0; j < szel; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
--- KovFer1010Feladatok/src/feladatok9.java
+++ KovFer1010Feladatok/src/feladatok9.java
@@ -6,7 +6,37 @@ public class feladatok9 {
 				+ "\n\tÍrj programot, amely kirajzolja az amerikai zászlót! "
 				+ "\n\t(A bal felső sarokban csillagok, a többi részen kötőjelek.) "
 				+ "\n\tA megoldáshoz használj egymásba ágyazott ciklusokat!");
-
+		
+		for (int i = 0; i < 10; i++) {
+			if (i==0||i==2||i==4){
+				for (int j = 0; j < 7; j++) {
+					System.out.print("* ");
+				}
+				for (int j = 0; j < 20; j++) {
+					System.out.print("==");
+				}
+				System.out.print("\n");
+			}
+			else if (i==1||i==3){
+				for (int j = 0; j < 6; j++) {
+					System.out.print(" *");
+				}
+				System.out.print("  ");
+				for (int j = 0; j < 20; j++) {
+					System.out.print("==");
+				}
+				System.out.print("\n");
+			}
+			else {
+				for (int j = 0; j < 27; j++) {
+					System.out.print("==");
+			}
+				System.out.print("\n");
+			
+		}
+		
+		}
 	}
-
 }
+
+
